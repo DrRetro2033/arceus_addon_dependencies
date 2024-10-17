14b12fe4d2ef6ec0e4428a26bbecf83dbe8b588e
DrRetro2033 <colindrwho@gmail.com>
2024-10-17 09:32:16 -0400

    Trying build_runner.

--- lib/builder.dart
+++ lib/builder.dart
@@ -1,43 +1,47 @@
-import 'dart:io';
+import 'dart:typed_data';
 import 'package:dart_eval/dart_eval.dart';
 import 'package:yaml/yaml.dart';
+import 'package:build/build.dart';
+import 'package:glob/glob.dart';
 
-void compile() {
-  final _compiler = Compiler();
-  final projectPath = Directory.current.path;
-  String addonName;
+class AddonBuilder implements Builder {
+  final compiler = Compiler();
 
-  final dir = Directory(projectPath);
-  Map<String, Map<String, String>> packages = {"project": {}};
-  final files = dir
-      .listSync(recursive: true)
-      .where((element) => element.path.endsWith('.dart'))
-      .map((e) => e.path)
-      .toList(); // Get all the dart files.
-
-  AddOnFingerprint y;
-  if (!dir
-      .listSync()
-      .map((e) => e.path)
-      .toList()
-      .any((e) => e.endsWith('addon.yaml') || e.endsWith('addon.yml'))) {
-    throw Exception(
-        "Cannot compile without an addon.yaml file in the project folder.");
-  } else {
-    File addonYaml = File('$projectPath/addon.yaml');
-    if (!addonYaml.existsSync()) {
-      addonYaml = File('$projectPath/addon.yml');
-    }
-    final addonYamlMap = loadYaml(addonYaml.readAsStringSync());
-    addonName = addonYamlMap['name'];
-    y = AddOnFingerprint.fromYaml(addonYamlMap);
+  @override
+  Future<void> build(BuildStep buildStep) async {
+    final inputFiles = await buildStep.findAssets(Glob("*.*")).toList();
+    await compile(buildStep, inputFiles);
   }
 
-  if (!files.any((e) => e.endsWith('main.dart'))) {
+  @override
+  Map<String, List<String>> get buildExtensions => {
+        '.dart': ['.evc']
+      };
+}
+
+Future<void> compile(BuildStep buildStep, List<AssetId> files) async {
+  final compiler = Compiler();
+  String addonName;
+  Map<String, Map<String, String>> packages = {"project": {}};
+  AddOnFingerprint y;
+  if (files.any((e) => e.path.endsWith('addon.yaml'))) {
+    y = AddOnFingerprint.fromYaml(
+      loadYaml(await buildStep.readAsString(files.firstWhere(
+        (e) => e.path.endsWith('addon.yaml'),
+      ))),
+    );
+    addonName = y.name;
+  } else {
+    throw Exception(
+        "Cannot compile without an addon.yaml file in the project folder.");
+  }
+
+  if (!files.any((e) => e.path.endsWith('main.dart'))) {
     throw Exception(
         "Cannot compile without a main.dart file in the project folder with read, write, and isCompatible functions.");
   } else {
-    final mainCode = File("$projectPath/main.dart").readAsStringSync();
+    final mainCode = await buildStep
+        .readAsString(files.firstWhere((e) => e.path.endsWith('main.dart')));
     if (!mainCode.contains("Map<String, dynamic> read(String file)") ||
         !mainCode
             .contains("void write(String file, Map<String, dynamic> data)") ||
@@ -48,20 +52,18 @@ void compile() {
   }
 
   for (var file in files) {
-    final code = File(file).readAsStringSync(); // Read the file.
-    packages["project"]?[file] = code; // Add the file to the package.
+    final code = await buildStep.readAsString(file);
+    packages["project"]?[file.path] = code; // Add the file to the package.
   }
 
-  final addon = _compiler.compile(packages); // The compiled code.
-  final addonFile = File('$projectPath/build/$addonName.evc');
-  addonFile.createSync(
-      recursive: true); // Create the compiled file for writing to.
-
-  addonFile.writeAsBytesSync(y.fingerprint.codeUnits,
-      mode: FileMode.write); // Write the fingerprint to disk.
-
-  addonFile.writeAsBytesSync(addon.write(),
-      mode: FileMode.append); // Write the compiled addon to disk.
+  final addon = compiler.compile(packages); // The compiled code.
+  final outputId = AssetId(buildStep.inputId.package, 'build/$addonName.evc');
+  final finalFile = y.fingerprint.codeUnits.toList();
+  finalFile.addAll(addon.write().toList());
+  await buildStep.writeAsBytes(
+    outputId,
+    Uint8List.fromList(finalFile),
+  );
 }
 
 class AddOnFingerprint {
